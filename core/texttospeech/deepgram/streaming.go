3c3b8c8c0e39aa18905936be1675f428a2e0f730
Luka Košćak <koscak.luka@gmail.com>
2026-01-30 10:36:11 +0200

    feat(tts): add SpeechGenerator interface + deepgram implementation

--- core/texttospeech/deepgram/streaming.go
+++ core/texttospeech/deepgram/streaming.go
@@ -3,18 +3,319 @@ package deepgram
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
 	"net/url"
 	"os"
 	"strconv"
+	"sync"
 
 	"github.com/gorilla/websocket"
 	"github.com/koscakluka/ema-core/core/audio"
 	"github.com/koscakluka/ema-core/core/texttospeech"
 )
 
+type streamingRequest struct {
+	ws *websocket.Conn
+	mu sync.Mutex
+
+	textBuffer   []string
+	textBufferMu sync.Mutex
+
+	options streamingRequestOptions
+
+	textComplete bool
+	cancelled    bool
+	closed       bool
+
+	report texttospeech.SpeechEndedReport
+}
+
+type streamingRequestOptions struct {
+	texttospeech.TextToSpeechOptions
+	Voice deepgramVoice
+}
+
+func (c *TextToSpeechClient) NewSpeechGeneratorV0(ctx context.Context, opts ...texttospeech.TextToSpeechOption) (texttospeech.SpeechGeneratorV0, error) {
+	req := &streamingRequest{
+		options: streamingRequestOptions{
+			TextToSpeechOptions: texttospeech.TextToSpeechOptions{
+				AudioCallback:         func([]byte) {},
+				AudioEnded:            func(string) {},
+				SpeechAudioCallback:   func([]byte) {},
+				SpeechMarkCallback:    func(string) {},
+				SpeechEndedCallbackV0: func(texttospeech.SpeechEndedReport) {},
+				ErrorCallback:         func(error) {},
+			},
+		},
+	}
+
+	for _, opt := range opts {
+		opt(&req.options.TextToSpeechOptions)
+	}
+
+	var err error
+	if req.ws, err = connectWebsocket(c.voice, c.options.EncodingInfo); err != nil {
+		return nil, fmt.Errorf("failed to open websocket: %w", err)
+	}
+
+	go req.processIncomingMessages(ctx)
+
+	return req, nil
+}
+
+func connectWebsocket(voice deepgramVoice, encodingInfo audio.EncodingInfo) (*websocket.Conn, error) {
+	// TODO: Allow passing API key in constructor
+	apiKey, ok := os.LookupEnv("DEEPGRAM_API_KEY")
+	if !ok {
+		return nil, fmt.Errorf("deepgram api key not found")
+	}
+
+	urlValues := url.Values{}
+	urlValues.Set("encoding", encodingInfo.Encoding)
+	urlValues.Set("sample_rate", strconv.Itoa(encodingInfo.SampleRate))
+	urlValues.Set("model", string(voice))
+	urlValues.Set("container", "none")
+
+	// TODO: Use DialContext
+	conn, _, err := websocket.DefaultDialer.Dial(
+		(&url.URL{
+			Scheme: "wss",
+			Host:   "api.deepgram.com", Path: "/v1/speak",
+			RawQuery: urlValues.Encode(),
+		}).String(),
+		http.Header{"Authorization": {"token " + apiKey}})
+	if err != nil {
+		return nil, fmt.Errorf("failed to open socket connection to deepgram: %w", err)
+	}
+
+	return conn, nil
+}
+
+func (r *streamingRequest) processIncomingMessages(ctx context.Context) {
+
+	// TODO: We can probably stop once we close or cancel
+	for {
+		msgType, msg, err := r.ws.ReadMessage()
+		if err != nil {
+			// TODO: Actually figure out this message instead of comparing to a string
+			if err.Error() != "websocket: close 1000 (normal)" {
+				log.Printf("Websocket read error: %v", err)
+			}
+			if err := r.Cancel(); err != nil {
+				_ = r.Close() // Ignored on purpose
+				return
+			}
+			return
+		}
+
+		switch msgType {
+		case websocket.BinaryMessage:
+			r.options.SpeechAudioCallback(msg)
+		case websocket.TextMessage:
+			var parsedMsg struct {
+				Type string `json:"type"`
+			}
+			if err := json.Unmarshal(msg, &parsedMsg); err != nil {
+				// TODO: Instrument
+				// log.Printf("Failed to unmarshal deepgram message: %v", err)
+				continue
+			}
+
+			switch parsedMsg.Type {
+			case "Flushed":
+				func() { // Grouped for defer
+					r.textBufferMu.Lock()
+					defer r.textBufferMu.Unlock()
+					// notify the user we have reached the mark
+					if len(r.textBuffer) > 0 {
+						r.options.SpeechMarkCallback(r.textBuffer[0])
+						r.textBuffer = r.textBuffer[1:]
+					}
+
+					// nothing left to process, nortify the user of the end
+					if len(r.textBuffer) == 0 && r.textComplete {
+						r.options.SpeechEndedCallbackV0(r.report)
+						_ = r.Close() // TODO: See if we need to react on this error
+						return
+					}
+
+					// send the next text if there is any
+					if len(r.textBuffer) > 0 {
+						if err := r.sendWebsocketMessage(sendTextMsg(r.textBuffer[0])); err != nil {
+							// TODO: Instrument
+							// log.Printf("Failed to speak deepgram text: %v", err)
+						}
+					}
+					// flush if there is even more thxt
+					if len(r.textBuffer) > 1 {
+						if err := r.sendWebsocketMessage(flushMsg); err != nil {
+							// TODO: Instrument
+							// log.Printf("Failed to flush deepgram buffer: %v", err)
+						}
+					}
+				}()
+			case "Clear":
+				// TODO: Handle clear message
+			case "Close":
+				// TODO: Handle close message
+			default:
+				// TODO: Handle unknown message types
+				// TODO: Instrument
+			}
+		case websocket.CloseMessage:
+			// TODO: Handle close message
+		case websocket.PingMessage:
+		// TODO: Handle ping message
+		case websocket.PongMessage:
+			// TODO: Handle pong message
+		default:
+			// TODO: Handle unknown message types
+			// TODO: Instrument
+		}
+	}
+}
+
+func (r *streamingRequest) SendText(text string) error {
+	if r.closed {
+		return fmt.Errorf("streaming request closed")
+	} else if r.cancelled {
+		return fmt.Errorf("streaming request cancelled")
+	} else if r.textComplete {
+		return fmt.Errorf("streaming request text already completed")
+	}
+
+	r.textBufferMu.Lock()
+	defer r.textBufferMu.Unlock()
+
+	if len(r.textBuffer) == 0 {
+		r.textBuffer = append(r.textBuffer, "")
+	}
+
+	if len(r.textBuffer) == 1 {
+		if err := r.sendWebsocketMessage(sendTextMsg(text)); err != nil {
+			return fmt.Errorf("failed to send websocket send text message: %w", err)
+		}
+	}
+	r.textBuffer[len(r.textBuffer)-1] += text
+	return nil
+}
+
+func (r *streamingRequest) Mark() error {
+	if r.closed {
+		return fmt.Errorf("streaming request closed")
+	} else if r.cancelled {
+		return fmt.Errorf("streaming request cancelled")
+	} else if r.textComplete {
+		return fmt.Errorf("streaming request text already completed")
+	}
+
+	r.textBufferMu.Lock()
+	defer r.textBufferMu.Unlock()
+
+	if len(r.textBuffer) == 1 {
+		if err := r.sendWebsocketMessage(flushMsg); err != nil {
+			return fmt.Errorf("failed to send websocket flush message: %w", err)
+		}
+	}
+
+	// NOTE: For some reason deepgram sometimes drops text that is passed after
+	// a flush unless there is some kind of break. This allows us to send the
+	// text after we get the flush confirmation
+	r.textBuffer = append(r.textBuffer, "")
+
+	return nil
+}
+
+func (r *streamingRequest) EndOfText() error {
+	if r.closed {
+		return fmt.Errorf("streaming request closed")
+	} else if r.cancelled {
+		return fmt.Errorf("streaming request cancelled")
+	}
+	r.textBufferMu.Lock()
+	defer r.textBufferMu.Unlock()
+
+	r.textComplete = true
+	if len(r.textBuffer) == 0 {
+		r.options.SpeechEndedCallbackV0(r.report)
+		_ = r.Close() // TODO: See if we need to react on this error
+	} else if len(r.textBuffer) == 1 && r.textBuffer[0] == "" {
+		r.textBuffer = r.textBuffer[1:]
+		r.options.SpeechEndedCallbackV0(r.report)
+		_ = r.Close() // TODO: See if we need to react on this error
+	}
+
+	return nil
+}
+
+func (r *streamingRequest) Cancel() error {
+	if r.closed {
+		return fmt.Errorf("streaming request closed")
+	}
+
+	r.cancelled = true
+	if err := r.sendWebsocketMessage(clearMsg); err != nil {
+		return fmt.Errorf("failed to send websocket close message: %w", err)
+	}
+
+	// TODO: This shoud technically be done once we have a confirmation
+	_ = r.Close()
+	return nil
+
+}
+
+func (r *streamingRequest) Close() error {
+	r.closed = true
+	if err := r.sendWebsocketMessage(closeMsg); err != nil {
+		if agressiveCloseErr := r.ws.Close(); agressiveCloseErr != nil {
+			return fmt.Errorf("failed to close websocket: %w", errors.Join(err, agressiveCloseErr))
+		}
+	}
+	return nil
+}
+
+type websocketMessage struct {
+	Type string `json:"type"`
+}
+
+var (
+	sendTextMsg = func(text string) struct {
+		Type string `json:"type"`
+		Text string `json:"text"`
+	} { // TODO: This is awkward, it should somehow be the same type
+		return struct {
+			Type string `json:"type"`
+			Text string `json:"text"`
+		}{Type: "Speak", Text: text}
+	}
+	flushMsg = websocketMessage{Type: "Flush"}
+	clearMsg = websocketMessage{Type: "Clear"}
+	closeMsg = websocketMessage{Type: "Close"}
+)
+
+func (r *streamingRequest) sendWebsocketMessage(msg any) error {
+	// TODO: Find a way to enforce a type instead of having any,
+	// specifically [websocketMessage] type
+	r.mu.Lock()
+	defer r.mu.Unlock()
+	if r.closed {
+		return fmt.Errorf("websocket connection closed")
+	} else if r.ws == nil {
+		return fmt.Errorf("websocket connection closed")
+	}
+
+	if err := r.ws.WriteJSON(msg); err != nil {
+		return fmt.Errorf("failed to write to websocket: %w", err)
+	}
+	return nil
+}
+
+// OpenStream opens a streaming connection to the TTS server
+//
+// Deprecated: (since v0.0.14) use [TextToSpeechClient].NewSpeechGeneratorV0 instead
 func (c *TextToSpeechClient) OpenStream(ctx context.Context, opts ...texttospeech.TextToSpeechOption) error {
 	for _, opt := range opts {
 		opt(&c.options)
@@ -32,32 +333,9 @@ func (c *TextToSpeechClient) OpenStream(ctx context.Context, opts ...texttospeec
 	return nil
 }
 
-func connectWebsocket(voice deepgramVoice, encodingInfo audio.EncodingInfo) (*websocket.Conn, error) {
-	apiKey, ok := os.LookupEnv("DEEPGRAM_API_KEY")
-	if !ok {
-		return nil, fmt.Errorf("deepgram api key not found")
-	}
-
-	urlValues := url.Values{}
-	urlValues.Set("encoding", encodingInfo.Encoding)
-	urlValues.Set("sample_rate", strconv.Itoa(encodingInfo.SampleRate))
-	urlValues.Set("model", string(voice))
-	urlValues.Set("container", "none")
-
-	conn, _, err := websocket.DefaultDialer.Dial(
-		(&url.URL{
-			Scheme: "wss",
-			Host:   "api.deepgram.com", Path: "/v1/speak",
-			RawQuery: urlValues.Encode(),
-		}).String(),
-		http.Header{"Authorization": {"token " + apiKey}})
-	if err != nil {
-		return nil, fmt.Errorf("failed to open socket connection to deepgram: %w", err)
-	}
-
-	return conn, err
-}
-
+// SendText sends text to the TTS server
+//
+// Deprecated: (since v0.0.14) use [texttospeech.SpeechGeneratorV0].SendText instead
 func (c *TextToSpeechClient) SendText(text string) error {
 	targetBuffer := &c.transcriptBuffer
 	if c.postRestartBuffer != nil {
@@ -100,6 +378,9 @@ func (c *TextToSpeechClient) speak(text string) error {
 	return nil
 }
 
+// FlushBuffer flushes the TTS buffer
+//
+// Deprecated: (since v0.0.14) use [texttospeech.SpeechGeneratorV0].Mark instead
 func (c *TextToSpeechClient) FlushBuffer() error {
 	if len(c.transcriptBuffer) == 1 {
 		if err := c.flush(); err != nil {
@@ -131,6 +412,9 @@ func (c *TextToSpeechClient) flush() error {
 	return nil
 }
 
+// ClearBuffer clears the TTS buffer
+//
+// Deprecated: (since v0.0.14) use [texttospeech.SpeechGeneratorV0].Cancel instead
 func (c *TextToSpeechClient) ClearBuffer() error {
 	if c.wsConn == nil {
 		return fmt.Errorf("connection closed")
@@ -147,6 +431,9 @@ func (c *TextToSpeechClient) ClearBuffer() error {
 	return nil
 }
 
+// CloseStream closes the streaming connection to the TTS server
+//
+// Deprecated: (since v0.0.14) use [texttospeech.SpeechGeneratorV0].Close instead
 func (c *TextToSpeechClient) CloseStream(ctx context.Context) error {
 	if c.wsConn != nil {
 		c.mu.Lock()
--- core/texttospeech/options.go
+++ core/texttospeech/options.go
@@ -3,24 +3,71 @@ package texttospeech
 import "github.com/koscakluka/ema-core/core/audio"
 
 type TextToSpeechOptions struct {
+	// AudioCallback is called when the TTS client produces audio
+	//
+	// Deprecated: (since v0.0.14) use SpeechAudioCallback instead
 	AudioCallback func(audio []byte)
-	AudioEnded    func(transcript string)
+	// SpeechAudioCallback is called when the TTS client produces audio
+	SpeechAudioCallback func(audio []byte)
+	// AudioEnded is called when the TTS client has finished producing audio
+	//
+	// Deprecated: (since v0.0.14) use SpeechMarkCallback instead
+	AudioEnded func(transcript string)
+	// SpeechMarkCallback is called when the TTS client produces speech until the
+	// marked text. Each mark is called once.
+	SpeechMarkCallback func(string)
+	// SpeechEndedCallbackV0 is called when the TTS client has finished producing speech
+	// and provides a report of the speech generation
+	SpeechEndedCallbackV0 func(SpeechEndedReport)
+	// ErrorCallback is called when the TTS client encounters an error, this usually
+	// means the TTS client has been cancelled
+	ErrorCallback func(error)
 
 	EncodingInfo audio.EncodingInfo
 }
 
 type TextToSpeechOption func(*TextToSpeechOptions)
 
-func WithAudioCallback(callback func(audio []byte)) TextToSpeechOption {
+// WithAudioCallback sets the callback for audio data
+//
+// Deprecated: (since v0.0.14) use [WithSpeechAudioCallback] instead
+func WithAudioCallback(callback func([]byte)) TextToSpeechOption {
 	return func(o *TextToSpeechOptions) {
 		o.AudioCallback = callback
+		o.SpeechAudioCallback = callback
 	}
 }
 
+func WithSpeechAudioCallback(callback func([]byte)) TextToSpeechOption {
+	return func(o *TextToSpeechOptions) {
+		o.AudioCallback = callback
+		o.SpeechAudioCallback = callback
+	}
+}
+
+// WithAudioEndedCallback sets the callback for when the TTS client has finished producing audio
+//
+// Deprecated: (since v0.0.14) use [WithSpeechMarkCallback] instead
 func WithAudioEndedCallback(callback func(transcript string)) TextToSpeechOption {
 	return func(o *TextToSpeechOptions) {
 		o.AudioEnded = callback
+		o.SpeechMarkCallback = callback
 	}
+}
+
+func WithSpeechMarkCallback(callback func(string)) TextToSpeechOption {
+	return func(o *TextToSpeechOptions) {
+		o.SpeechMarkCallback = callback
+		o.AudioEnded = callback
+	}
+}
+
+func WithSpeechEndedCallbackV0(callback func(SpeechEndedReport)) TextToSpeechOption {
+	return func(o *TextToSpeechOptions) { o.SpeechEndedCallbackV0 = callback }
+}
+
+func WithErrorCallback(callback func(error)) TextToSpeechOption {
+	return func(o *TextToSpeechOptions) { o.ErrorCallback = callback }
 }
 
 func WithEncodingInfo(encodingInfo audio.EncodingInfo) TextToSpeechOption {
@@ -33,3 +80,45 @@ func WithEncodingInfo(encodingInfo audio.EncodingInfo) TextToSpeechOption {
 		o.EncodingInfo = encodingInfo
 	}
 }
+
+type SpeechGeneratorV0 interface {
+	// SendText sends text to [SpeechGenerator]. It is guaranteed that the
+	// speech will be generated in the order text is sent.
+	//
+	// SendText will error if EndOfText, Cancel or Close has been called.
+	SendText(string) error
+	// Mark marks the current point in the text. It is guaranteed that the mark
+	// will be returned after the text sent up to the mark has been generated.
+	// There is no guarantee that the mark will be returned exactly at the point
+	// where it was marked.
+	//
+	// Mark will error if EndOfText, Cancel or Close has been called.
+	Mark() error
+	// EndOfText sends a signal to the [SpeechGenerator]that no more text will
+	// be sent. After EndOfText is called, [SpeechGenerator] will Close after
+	// all the speech has been generated.
+	//
+	// EndOfText will error if Cancel or Close has been called.
+	// Repeated calls to EndOfText are ignored.
+	EndOfText() error
+	// Cancel immediately cancels the further speech generation. It also closes
+	// [SpeechGenerator].
+	//
+	// This will error if Close has been called.
+	// Repeated calls to Cancel are ignored.
+	Cancel() error
+	// Close immediately closes the [SpeechGenerator]. It is guaranteed that no
+	// more speech will be generated after this call.
+	//
+	// Repeated calls to Close are ignored.
+	Close() error
+}
+
+type SpeechEndedReport struct{}
+
+// TODO: Extend the report with more information
+// type SpeechEndedPosition struct {
+// 	Text          string
+// 	StartPosition int
+// 	EndPosition   int
+// }
